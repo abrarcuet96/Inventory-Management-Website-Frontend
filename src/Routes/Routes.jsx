c2c17858235e98365ef37c667798ac1c9a0ad216
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-11-26 11:36:16 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const myRoutes= createBrowserRouter([
+    {
+        path: '/',
+        element: <div><h2>Start Project</h2></div>
+    }
+])
+export default myRoutes;
